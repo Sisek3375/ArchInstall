eaad0947f07094202106099a3954735c1a7d2a6b
Sisek3375 <corvisiersimon6@gmail.com>
2025-02-14 14:34:37 +0100

    add features

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -69,7 +69,7 @@ sed -i 's/\(HOOKS=(.*\)filesystems/\1lvm2 filesystems/' /etc/mkinitcpio.conf
 mkinitcpio -P
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=CustomArchBootLoader
 grub-mkconfig -o /boot/grub/grub.cfg
-exit
+# exit
 
 # Enable network card
 systemctl enable systemd-networkd systemd-resolved
@@ -83,4 +83,4 @@ EOF
 
 umount -R /mnt
 
-reboot
+# reboot
